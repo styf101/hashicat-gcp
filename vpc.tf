08907be8b2c04582c4dd99ffa2f14cbdb4391194
Your Name <you@example.com>
2023-11-01 19:17:10 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 module "network" {
   source       = "app.terraform.io/TestLab101/network/google"
   version      = "3.4.0"
-  network_name = "${var.prefix}-vpc-${var.region}"
+  network_name = "gau-network"
   project_id   = var.project
   subnets = [
     {
-      subnet_name   = "${var.prefix}-subnet"
+      subnet_name   = "gau-subnet"
       subnet_ip     = "10.10.10.0/24"
       subnet_region = var.region
     }
